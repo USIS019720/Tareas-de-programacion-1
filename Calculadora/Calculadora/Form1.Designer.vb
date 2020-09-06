fb602194286d3906b03a9e59a1b0f2415b12d9b7
Billy Josue Villanueva Abrego <usis002320@ugb.edu.sv>
2020-09-05 19:49:17 -0600

    Se realizó la conversión de manzanas y hectareas

--- Calculadora/Calculadora/Form1.Designer.vb
+++ Calculadora/Calculadora/Form1.Designer.vb
@@ -1,9 +1,9 @@
-﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
 Partial Class Calculadora
     Inherits System.Windows.Forms.Form
 
     'Form reemplaza a Dispose para limpiar la lista de componentes.
-    <System.Diagnostics.DebuggerNonUserCode()> _
+    <System.Diagnostics.DebuggerNonUserCode()>
     Protected Overrides Sub Dispose(ByVal disposing As Boolean)
         Try
             If disposing AndAlso components IsNot Nothing Then
@@ -20,7 +20,7 @@ Partial Class Calculadora
     'NOTA: el Diseñador de Windows Forms necesita el siguiente procedimiento
     'Se puede modificar usando el Diseñador de Windows Forms.  
     'No lo modifique con el editor de código.
-    <System.Diagnostics.DebuggerStepThrough()> _
+    <System.Diagnostics.DebuggerStepThrough()>
     Private Sub InitializeComponent()
         Me.Universal = New System.Windows.Forms.TabControl()
         Me.TabPage1 = New System.Windows.Forms.TabPage()
@@ -33,28 +33,34 @@ Partial Class Calculadora
         Me.Label2 = New System.Windows.Forms.Label()
         Me.Label1 = New System.Windows.Forms.Label()
         Me.TabPage2 = New System.Windows.Forms.TabPage()
+        Me.Button3 = New System.Windows.Forms.Button()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.Label8 = New System.Windows.Forms.Label()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.GroupBox1 = New System.Windows.Forms.GroupBox()
+        Me.lblr = New System.Windows.Forms.Label()
+        Me.lblum = New System.Windows.Forms.Label()
         Me.cmdconvertir = New System.Windows.Forms.Button()
         Me.cbxsalida = New System.Windows.Forms.ComboBox()
         Me.cbxentrada = New System.Windows.Forms.ComboBox()
         Me.Label7 = New System.Windows.Forms.Label()
         Me.Label6 = New System.Windows.Forms.Label()
-        Me.txtvalor = New System.Windows.Forms.Label()
-        Me.TextBox = New System.Windows.Forms.TextBox()
-        Me.lblr = New System.Windows.Forms.Label()
-        Me.lblum = New System.Windows.Forms.Label()
+        Me.label33 = New System.Windows.Forms.Label()
+        Me.txtvalor = New System.Windows.Forms.TextBox()
         Me.Universal.SuspendLayout()
         Me.TabPage1.SuspendLayout()
         Me.TabPage2.SuspendLayout()
+        Me.GroupBox1.SuspendLayout()
         Me.SuspendLayout()
         '
         'Universal
         '
         Me.Universal.Controls.Add(Me.TabPage1)
         Me.Universal.Controls.Add(Me.TabPage2)
-        Me.Universal.Location = New System.Drawing.Point(57, 12)
+        Me.Universal.Location = New System.Drawing.Point(0, 0)
         Me.Universal.Name = "Universal"
         Me.Universal.SelectedIndex = 0
-        Me.Universal.Size = New System.Drawing.Size(536, 309)
+        Me.Universal.Size = New System.Drawing.Size(596, 428)
         Me.Universal.TabIndex = 0
         '
         'TabPage1
@@ -67,10 +73,11 @@ Partial Class Calculadora
         Me.TabPage1.Controls.Add(Me.Label3)
         Me.TabPage1.Controls.Add(Me.Label2)
         Me.TabPage1.Controls.Add(Me.Label1)
+        Me.TabPage1.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
         Me.TabPage1.Location = New System.Drawing.Point(4, 22)
         Me.TabPage1.Name = "TabPage1"
         Me.TabPage1.Padding = New System.Windows.Forms.Padding(3)
-        Me.TabPage1.Size = New System.Drawing.Size(528, 283)
+        Me.TabPage1.Size = New System.Drawing.Size(588, 402)
         Me.TabPage1.TabIndex = 0
         Me.TabPage1.Text = "Universal"
         Me.TabPage1.UseVisualStyleBackColor = True
@@ -88,21 +95,21 @@ Partial Class Calculadora
         '
         Me.TextBox3.Location = New System.Drawing.Point(121, 136)
         Me.TextBox3.Name = "TextBox3"
-        Me.TextBox3.Size = New System.Drawing.Size(130, 20)
+        Me.TextBox3.Size = New System.Drawing.Size(130, 21)
         Me.TextBox3.TabIndex = 14
         '
         'TextBox2
         '
         Me.TextBox2.Location = New System.Drawing.Point(323, 56)
         Me.TextBox2.Name = "TextBox2"
-        Me.TextBox2.Size = New System.Drawing.Size(148, 20)
+        Me.TextBox2.Size = New System.Drawing.Size(148, 21)
         Me.TextBox2.TabIndex = 13
         '
         'TextBox1
         '
         Me.TextBox1.Location = New System.Drawing.Point(112, 56)
         Me.TextBox1.Name = "TextBox1"
-        Me.TextBox1.Size = New System.Drawing.Size(130, 20)
+        Me.TextBox1.Size = New System.Drawing.Size(130, 21)
         Me.TextBox1.TabIndex = 12
         '
         'Label4
@@ -110,78 +117,165 @@ Partial Class Calculadora
         Me.Label4.AutoSize = True
         Me.Label4.Location = New System.Drawing.Point(278, 139)
         Me.Label4.Name = "Label4"
-        Me.Label4.Size = New System.Drawing.Size(0, 13)
+        Me.Label4.Size = New System.Drawing.Size(0, 15)
         Me.Label4.TabIndex = 11
         '
         'Label3
         '
         Me.Label3.AutoSize = True
-        Me.Label3.Location = New System.Drawing.Point(57, 139)
+        Me.Label3.Location = New System.Drawing.Point(42, 139)
         Me.Label3.Name = "Label3"
-        Me.Label3.Size = New System.Drawing.Size(58, 13)
+        Me.Label3.Size = New System.Drawing.Size(75, 15)
         Me.Label3.TabIndex = 10
         Me.Label3.Text = "Respuesta"
         '
         'Label2
         '
         Me.Label2.AutoSize = True
-        Me.Label2.Location = New System.Drawing.Point(265, 59)
+        Me.Label2.Location = New System.Drawing.Point(249, 59)
         Me.Label2.Name = "Label2"
-        Me.Label2.Size = New System.Drawing.Size(52, 13)
+        Me.Label2.Size = New System.Drawing.Size(68, 15)
         Me.Label2.TabIndex = 9
         Me.Label2.Text = "Unidades"
         '
         'Label1
         '
         Me.Label1.AutoSize = True
-        Me.Label1.Location = New System.Drawing.Point(57, 59)
+        Me.Label1.Location = New System.Drawing.Point(42, 59)
         Me.Label1.Name = "Label1"
-        Me.Label1.Size = New System.Drawing.Size(49, 13)
+        Me.Label1.Size = New System.Drawing.Size(64, 15)
         Me.Label1.TabIndex = 8
         Me.Label1.Text = "Cantidad"
         '
         'TabPage2
         '
-        Me.TabPage2.Controls.Add(Me.lblum)
-        Me.TabPage2.Controls.Add(Me.lblr)
+        Me.TabPage2.BackColor = System.Drawing.Color.FromArgb(CType(CType(255, Byte), Integer), CType(CType(192, Byte), Integer), CType(CType(128, Byte), Integer))
+        Me.TabPage2.Controls.Add(Me.Button3)
+        Me.TabPage2.Controls.Add(Me.Button2)
+        Me.TabPage2.Controls.Add(Me.Label8)
+        Me.TabPage2.Controls.Add(Me.Label5)
+        Me.TabPage2.Controls.Add(Me.GroupBox1)
         Me.TabPage2.Controls.Add(Me.cmdconvertir)
         Me.TabPage2.Controls.Add(Me.cbxsalida)
         Me.TabPage2.Controls.Add(Me.cbxentrada)
         Me.TabPage2.Controls.Add(Me.Label7)
         Me.TabPage2.Controls.Add(Me.Label6)
+        Me.TabPage2.Controls.Add(Me.label33)
         Me.TabPage2.Controls.Add(Me.txtvalor)
-        Me.TabPage2.Controls.Add(Me.TextBox)
+        Me.TabPage2.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
         Me.TabPage2.Location = New System.Drawing.Point(4, 22)
         Me.TabPage2.Name = "TabPage2"
         Me.TabPage2.Padding = New System.Windows.Forms.Padding(3)
-        Me.TabPage2.Size = New System.Drawing.Size(528, 283)
+        Me.TabPage2.Size = New System.Drawing.Size(588, 402)
         Me.TabPage2.TabIndex = 1
         Me.TabPage2.Text = "Area(Superficie)"
-        Me.TabPage2.UseVisualStyleBackColor = True
+        '
+        'Button3
+        '
+        Me.Button3.BackColor = System.Drawing.Color.FromArgb(CType(CType(192, Byte), Integer), CType(CType(192, Byte), Integer), CType(CType(255, Byte), Integer))
+        Me.Button3.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button3.Location = New System.Drawing.Point(47, 191)
+        Me.Button3.Name = "Button3"
+        Me.Button3.Size = New System.Drawing.Size(81, 128)
+        Me.Button3.TabIndex = 13
+        Me.Button3.Text = "Borrar "
+        Me.Button3.UseVisualStyleBackColor = False
+        '
+        'Button2
+        '
+        Me.Button2.BackColor = System.Drawing.Color.FromArgb(CType(CType(255, Byte), Integer), CType(CType(192, Byte), Integer), CType(CType(192, Byte), Integer))
+        Me.Button2.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button2.Location = New System.Drawing.Point(132, 191)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(79, 128)
+        Me.Button2.TabIndex = 12
+        Me.Button2.Text = "Salir"
+        Me.Button2.UseVisualStyleBackColor = False
+        '
+        'Label8
+        '
+        Me.Label8.AutoSize = True
+        Me.Label8.BackColor = System.Drawing.Color.LightCoral
+        Me.Label8.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label8.Location = New System.Drawing.Point(385, 88)
+        Me.Label8.Name = "Label8"
+        Me.Label8.Size = New System.Drawing.Size(165, 16)
+        Me.Label8.TabIndex = 11
+        Me.Label8.Text = "Areas Seleccionables "
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.BackColor = System.Drawing.Color.SkyBlue
+        Me.Label5.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label5.Location = New System.Drawing.Point(199, 15)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(183, 20)
+        Me.Label5.TabIndex = 10
+        Me.Label5.Text = "Convertidor de Areas "
+        '
+        'GroupBox1
+        '
+        Me.GroupBox1.BackColor = System.Drawing.Color.Silver
+        Me.GroupBox1.Controls.Add(Me.lblr)
+        Me.GroupBox1.Controls.Add(Me.lblum)
+        Me.GroupBox1.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.GroupBox1.Location = New System.Drawing.Point(280, 228)
+        Me.GroupBox1.Name = "GroupBox1"
+        Me.GroupBox1.Size = New System.Drawing.Size(248, 57)
+        Me.GroupBox1.TabIndex = 9
+        Me.GroupBox1.TabStop = False
+        Me.GroupBox1.Text = "Respuesta"
+        '
+        'lblr
+        '
+        Me.lblr.AutoSize = True
+        Me.lblr.Font = New System.Drawing.Font("Arial Rounded MT Bold", 11.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblr.Location = New System.Drawing.Point(82, 28)
+        Me.lblr.Name = "lblr"
+        Me.lblr.Size = New System.Drawing.Size(17, 17)
+        Me.lblr.TabIndex = 7
+        Me.lblr.Text = "0"
+        '
+        'lblum
+        '
+        Me.lblum.AutoSize = True
+        Me.lblum.Font = New System.Drawing.Font("Arial Rounded MT Bold", 11.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblum.Location = New System.Drawing.Point(59, 28)
+        Me.lblum.Name = "lblum"
+        Me.lblum.Size = New System.Drawing.Size(17, 17)
+        Me.lblum.TabIndex = 8
+        Me.lblum.Text = "0"
         '
         'cmdconvertir
         '
-        Me.cmdconvertir.Location = New System.Drawing.Point(132, 119)
+        Me.cmdconvertir.BackColor = System.Drawing.Color.YellowGreen
+        Me.cmdconvertir.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.cmdconvertir.Location = New System.Drawing.Point(47, 145)
         Me.cmdconvertir.Name = "cmdconvertir"
-        Me.cmdconvertir.Size = New System.Drawing.Size(133, 67)
+        Me.cmdconvertir.Size = New System.Drawing.Size(166, 40)
         Me.cmdconvertir.TabIndex = 6
         Me.cmdconvertir.Text = "Convertir"
-        Me.cmdconvertir.UseVisualStyleBackColor = True
+        Me.cmdconvertir.UseVisualStyleBackColor = False
         '
         'cbxsalida
         '
+        Me.cbxsalida.BackColor = System.Drawing.SystemColors.ActiveCaption
+        Me.cbxsalida.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
         Me.cbxsalida.FormattingEnabled = True
-        Me.cbxsalida.Items.AddRange(New Object() {"Pie Cuadrado", "Vara Cuadrada", "Yarda Cuadrada", "Metro Cuadrado", "Tareas", "Manzanas", "Hectareas"})
-        Me.cbxsalida.Location = New System.Drawing.Point(210, 77)
+        Me.cbxsalida.Items.AddRange(New Object() {"Pie Cuadrado", "Vara Cuadrada", "Yarda Cuadrada", "Metro Cuadrado", "Tareas", "Manzana", "Hectarea"})
+        Me.cbxsalida.Location = New System.Drawing.Point(407, 167)
         Me.cbxsalida.Name = "cbxsalida"
         Me.cbxsalida.Size = New System.Drawing.Size(121, 21)
         Me.cbxsalida.TabIndex = 5
         '
         'cbxentrada
         '
+        Me.cbxentrada.BackColor = System.Drawing.SystemColors.ActiveCaption
+        Me.cbxentrada.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
         Me.cbxentrada.FormattingEnabled = True
-        Me.cbxentrada.Items.AddRange(New Object() {"Pie Cuadrado", "Vara Cuadrada", "Yarda Cuadrada", "Metro Cuadrado", "Tareas", "Manzanas", "Hectareas"})
-        Me.cbxentrada.Location = New System.Drawing.Point(36, 77)
+        Me.cbxentrada.Items.AddRange(New Object() {"Pie Cuadrado", "Vara Cuadrada", "Yarda Cuadrada", "Metro Cuadrado", "Tareas", "Manzana", "Hectarea"})
+        Me.cbxentrada.Location = New System.Drawing.Point(407, 121)
         Me.cbxentrada.Name = "cbxentrada"
         Me.cbxentrada.Size = New System.Drawing.Size(121, 21)
         Me.cbxentrada.TabIndex = 4
@@ -189,68 +283,57 @@ Partial Class Calculadora
         'Label7
         '
         Me.Label7.AutoSize = True
-        Me.Label7.Location = New System.Drawing.Point(187, 79)
+        Me.Label7.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label7.Location = New System.Drawing.Point(376, 167)
         Me.Label7.Name = "Label7"
-        Me.Label7.Size = New System.Drawing.Size(17, 13)
+        Me.Label7.Size = New System.Drawing.Size(19, 15)
         Me.Label7.TabIndex = 3
         Me.Label7.Text = "A:"
         '
         'Label6
         '
         Me.Label6.AutoSize = True
-        Me.Label6.Location = New System.Drawing.Point(6, 85)
+        Me.Label6.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label6.Location = New System.Drawing.Point(369, 127)
         Me.Label6.Name = "Label6"
-        Me.Label6.Size = New System.Drawing.Size(24, 13)
+        Me.Label6.Size = New System.Drawing.Size(29, 15)
         Me.Label6.TabIndex = 2
         Me.Label6.Text = "De:"
         '
+        'label33
+        '
+        Me.label33.AutoSize = True
+        Me.label33.BackColor = System.Drawing.Color.Plum
+        Me.label33.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.label33.Location = New System.Drawing.Point(94, 88)
+        Me.label33.Name = "label33"
+        Me.label33.Size = New System.Drawing.Size(70, 16)
+        Me.label33.TabIndex = 1
+        Me.label33.Text = "Cantidad"
+        '
         'txtvalor
         '
-        Me.txtvalor.AutoSize = True
-        Me.txtvalor.Location = New System.Drawing.Point(6, 33)
+        Me.txtvalor.Location = New System.Drawing.Point(47, 107)
         Me.txtvalor.Name = "txtvalor"
-        Me.txtvalor.Size = New System.Drawing.Size(49, 13)
-        Me.txtvalor.TabIndex = 1
-        Me.txtvalor.Text = "Cantidad"
-        '
-        'TextBox
-        '
-        Me.TextBox.Location = New System.Drawing.Point(61, 30)
-        Me.TextBox.Name = "TextBox"
-        Me.TextBox.Size = New System.Drawing.Size(100, 20)
-        Me.TextBox.TabIndex = 0
-        '
-        'lblr
-        '
-        Me.lblr.AutoSize = True
-        Me.lblr.Location = New System.Drawing.Point(350, 157)
-        Me.lblr.Name = "lblr"
-        Me.lblr.Size = New System.Drawing.Size(13, 13)
-        Me.lblr.TabIndex = 7
-        Me.lblr.Text = "0"
-        '
-        'lblum
-        '
-        Me.lblum.AutoSize = True
-        Me.lblum.Location = New System.Drawing.Point(265, 225)
-        Me.lblum.Name = "lblum"
-        Me.lblum.Size = New System.Drawing.Size(13, 13)
-        Me.lblum.TabIndex = 8
-        Me.lblum.Text = "0"
+        Me.txtvalor.Size = New System.Drawing.Size(166, 21)
+        Me.txtvalor.TabIndex = 0
         '
         'Calculadora
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.BackColor = System.Drawing.SystemColors.HotTrack
+        Me.ClientSize = New System.Drawing.Size(596, 428)
         Me.Controls.Add(Me.Universal)
         Me.Name = "Calculadora"
-        Me.Text = "Form1"
+        Me.Text = "Parcial 1"
         Me.Universal.ResumeLayout(False)
         Me.TabPage1.ResumeLayout(False)
         Me.TabPage1.PerformLayout()
         Me.TabPage2.ResumeLayout(False)
         Me.TabPage2.PerformLayout()
+        Me.GroupBox1.ResumeLayout(False)
+        Me.GroupBox1.PerformLayout()
         Me.ResumeLayout(False)
 
     End Sub
@@ -271,8 +354,13 @@ Partial Class Calculadora
     Friend WithEvents cbxentrada As ComboBox
     Friend WithEvents Label7 As Label
     Friend WithEvents Label6 As Label
-    Friend WithEvents txtvalor As Label
-    Friend WithEvents TextBox As TextBox
+    Friend WithEvents label33 As Label
+    Friend WithEvents txtvalor As TextBox
     Friend WithEvents lblum As Label
     Friend WithEvents lblr As Label
+    Friend WithEvents Button3 As Button
+    Friend WithEvents Button2 As Button
+    Friend WithEvents Label8 As Label
+    Friend WithEvents Label5 As Label
+    Friend WithEvents GroupBox1 As GroupBox
 End Class
