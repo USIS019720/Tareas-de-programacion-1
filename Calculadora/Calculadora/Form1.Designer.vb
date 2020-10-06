9c677ba9049cfa107e63f60a083200664feaa142
David <David@DESKTOP-RE9B29S>
2020-10-05 22:29:43 -0600

    se empezo a crear la base de datos

--- Calculadora/Calculadora/Form1.Designer.vb
+++ Calculadora/Calculadora/Form1.Designer.vb
@@ -1,9 +1,9 @@
-﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
 Partial Class Calculadora
     Inherits System.Windows.Forms.Form
 
     'Form reemplaza a Dispose para limpiar la lista de componentes.
-    <System.Diagnostics.DebuggerNonUserCode()> _
+    <System.Diagnostics.DebuggerNonUserCode()>
     Protected Overrides Sub Dispose(ByVal disposing As Boolean)
         Try
             If disposing AndAlso components IsNot Nothing Then
@@ -20,65 +20,18 @@ Partial Class Calculadora
     'NOTA: el Diseñador de Windows Forms necesita el siguiente procedimiento
     'Se puede modificar usando el Diseñador de Windows Forms.  
     'No lo modifique con el editor de código.
-    <System.Diagnostics.DebuggerStepThrough()> _
+    <System.Diagnostics.DebuggerStepThrough()>
     Private Sub InitializeComponent()
-        Me.btnProcesar = New System.Windows.Forms.Button()
-        Me.Label1 = New System.Windows.Forms.Label()
-        Me.lstTablas = New System.Windows.Forms.CheckedListBox()
-        Me.txtntabla = New System.Windows.Forms.TextBox()
         Me.SuspendLayout()
-        '
-        'btnProcesar
-        '
-        Me.btnProcesar.Location = New System.Drawing.Point(216, 110)
-        Me.btnProcesar.Name = "btnProcesar"
-        Me.btnProcesar.Size = New System.Drawing.Size(116, 44)
-        Me.btnProcesar.TabIndex = 7
-        Me.btnProcesar.Text = "Generar Tablas"
-        Me.btnProcesar.UseVisualStyleBackColor = True
-        '
-        'Label1
-        '
-        Me.Label1.AutoSize = True
-        Me.Label1.Location = New System.Drawing.Point(196, 37)
-        Me.Label1.Name = "Label1"
-        Me.Label1.Size = New System.Drawing.Size(30, 13)
-        Me.Label1.TabIndex = 6
-        Me.Label1.Text = "tabla"
-        '
-        'lstTablas
-        '
-        Me.lstTablas.FormattingEnabled = True
-        Me.lstTablas.Location = New System.Drawing.Point(338, 34)
-        Me.lstTablas.Name = "lstTablas"
-        Me.lstTablas.Size = New System.Drawing.Size(214, 274)
-        Me.lstTablas.TabIndex = 5
-        '
-        'txtntabla
-        '
-        Me.txtntabla.Location = New System.Drawing.Point(232, 34)
-        Me.txtntabla.Name = "txtntabla"
-        Me.txtntabla.Size = New System.Drawing.Size(100, 20)
-        Me.txtntabla.TabIndex = 4
         '
         'Calculadora
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.ClientSize = New System.Drawing.Size(800, 450)
-        Me.Controls.Add(Me.btnProcesar)
-        Me.Controls.Add(Me.Label1)
-        Me.Controls.Add(Me.lstTablas)
-        Me.Controls.Add(Me.txtntabla)
         Me.Name = "Calculadora"
         Me.Text = "Form1"
         Me.ResumeLayout(False)
-        Me.PerformLayout()
 
     End Sub
-
-    Friend WithEvents btnProcesar As Button
-    Friend WithEvents Label1 As Label
-    Friend WithEvents lstTablas As CheckedListBox
-    Friend WithEvents txtntabla As TextBox
 End Class
--- Calculadora/Calculadora/Form1.vb
+++ Calculadora/Calculadora/Form1.vb
@@ -1,12 +1,4 @@
 ﻿Public Class Calculadora
-    Private Sub btnCalcular_Click(sender As Object, e As EventArgs)
-        Dim ntabla As SByte
-        ntabla = txtntabla.Text
 
-        For i = 1 To 10
-            lstTablas.Items.Add(ntabla.ToString() + "x" + i.ToString() + "=" + (ntabla * i).ToString())
-        Next
-
-    End Sub
 
 End Class
